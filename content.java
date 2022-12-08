9ec367fca34c6d9e2a1588acfcc770264eaf5ac9
Minh Dang <mdang@ea.com>
2022-12-08 20:00:30 +0000

    add code in files types: .xml,.java,.yml,.groovy, remove code in files types: .xml,.java,.groovy

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Ciccwi {
     public static void main() {
         System.out.println("lvhoc");
         System.out.println("gavgy");
-        System.out.println("dcvgj");
+        System.out.println("fgxig");
     }
 }
